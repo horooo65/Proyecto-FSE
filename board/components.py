977e209db572f6054df90c905e70a7c788a04965
Horacio Acosta <horooo65@gmail.com>
2021-12-17 13:17:46 -0600

    Add files via upload

---
+++ board/components.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+# ## ###############################################
+#
+# Autores: 
+# Acosta Hernández Horacio Emmanuel
+# Juárez Munguía Brandon Jesús
+# Medina Cruz Josué Emanuel
+# License: MIT
+#
+# ## ###############################################
+
+# Future imports (Python 2.7 compatibility)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import sys
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
